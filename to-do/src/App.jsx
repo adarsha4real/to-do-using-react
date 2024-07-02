6186c3860c0d02cbe21204bf39f08c6b1141e810
adarsha <adarsha20582003@gmail.com>
2024-07-02 23:53:51 +0545

    finished layout using jsx and css

---
+++ to-do/src/App.jsx
@@ -0,0 +1,193 @@
+import React from 'react'
+import "./App.css"
+
+function App() {
+  return (
+      <main>
+      <center>
+        <h2>TO-DO APP</h2> 
+      </center>
+    
+      <form>
+          <div>
+                <input type="text" placeholder='Enter task' />
+                <select >
+                    <option>morning</option>
+                    <option>office</option>
+                    <option>growth</option>
+                    <option >night</option>
+                </select>
+          </div>
+          
+            
+              <button>click me</button>
+      </form>
+      
+
+      <div id="output">
+        <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  <div className="card">
+            <h3>
+              TaskName
+            </h3>
+          <div >
+             <p>Category</p>
+              <span>
+                    <label htmlFor="">Done</label>
+                    <input type="checkbox" />
+              </span>
+              <button>Delete</button>
+           </div>
+      </div>
+  
+
+      </div>
+
+
+    </main>
+  )
+}
+
+export default App
